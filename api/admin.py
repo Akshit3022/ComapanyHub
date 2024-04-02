d89564fa2de27b41e561b996c9abc885f06c3d29
Akshit3022 <akshit.chauhan@yudiz.com>
2024-04-02 17:19:59 +0530

    Added ReadOnly View

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from api.models import *
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('companyName', 'companyType')
+admin.site.register(Company, CompanyAdmin)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('employeeName', 'employeeEmail', 'employeeAddress', 'employeeContact', 'employeeAbout')
+admin.site.register(Employee, EmployeeAdmin)
 
 # Register your models here.
--- api/views.py
+++ api/views.py
@@ -2,12 +2,27 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from api.models import *
 from api.serializers import *
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 # Create your views here.
 
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+
+    # companies/company_id/employees
+    @action(detail=True, methods=['GET'])
+    def employees(self, request, pk=None):
+        try:
+            company = Company.objects.get(pk=pk)
+            emps = Employee.objects.filter(companyDetails=company)
+            emps_serializer = EmployeeSerializer(emps, many=True, context={'request': request})
+            return Response(emps_serializer.data)
+        except Exception as e:
+            print(e)
+            return Response({'message':'Company might not exists !!!'})
+
 
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
