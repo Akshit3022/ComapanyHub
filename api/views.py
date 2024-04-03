ac79fddd76d9d534b79c438b13585e43679ac9a0
Akshit3022 <akshit.chauhan@yudiz.com>
2024-04-03 14:10:30 +0530

    only admin can add, update and delete any company or employee, unauthorized user has only ReadOnly access.

--- api/views.py
+++ api/views.py
@@ -4,14 +4,42 @@ from api.models import *
 from api.serializers import *
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework import permissions
 
 # Create your views here.
+
+# class CompanyViewSet(viewsets.ModelViewSet):
+#     queryset = Company.objects.all()
+#     serializer_class = CompanySerializer
+
+#     # companies/company_id/employees
+#     @action(detail=True, methods=['GET'])
+#     def employees(self, request, pk=None):
+#         try:
+#             company = Company.objects.get(pk=pk)
+#             emps = Employee.objects.filter(companyDetails=company)
+#             emps_serializer = EmployeeSerializer(emps, many=True, context={'request': request})
+#             return Response(emps_serializer.data)
+#         except Exception as e:
+#             print(e)
+#             return Response({'message':'Company might not exists !!!'})
+
+
+# class EmployeeViewSet(viewsets.ModelViewSet):
+#     queryset = Employee.objects.all()
+#     serializer_class = EmployeeSerializer
+
 
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+    permission_classes = [permissions.IsAdminUser]  # Only admin can create, update, delete
 
-    # companies/company_id/employees
+    def get_permissions(self):
+        if self.action == 'list' or self.action == 'retrieve':
+            return [permissions.AllowAny()]  # Any user can read
+        return super().get_permissions()
+
     @action(detail=True, methods=['GET'])
     def employees(self, request, pk=None):
         try:
@@ -27,3 +55,10 @@ class CompanyViewSet(viewsets.ModelViewSet):
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return [permissions.IsAdminUser()]  # Only admin user can create
+        elif self.action == 'list' or self.action == 'retrieve':
+            return [permissions.AllowAny()]  # Any user can read
+        return [permissions.IsAdminUser()]  # Only admin can update, delete
--- companyApi/settings.py
+++ companyApi/settings.py
@@ -125,10 +125,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
-}
+# REST_FRAMEWORK = {
+#     # Use Django's standard `django.contrib.auth` permissions,
+#     # or allow read-only access for unauthenticated users.
+#     'DEFAULT_PERMISSION_CLASSES': [
+#         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+#     ]
+# }
